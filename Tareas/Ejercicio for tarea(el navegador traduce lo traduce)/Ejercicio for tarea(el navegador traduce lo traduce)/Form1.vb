239401c16b66e340f17a373ea2fbf42a9d115dd5
Mariocalix2001 <65907729+Mariocalix2001@users.noreply.github.com>
2020-05-31 22:23:54 -0600

    Add files via upload

---
+++ Tareas/Ejercicio for tarea(el navegador traduce lo traduce)/Ejercicio for tarea(el navegador traduce lo traduce)/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer
+        For i = 0 To 10 Step 1
+            MsgBox("Variable Acumuladora =" & i)
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Close()
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
